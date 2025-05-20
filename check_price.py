9d04671e812fc56d95c79744a0378d463db5b31e
Jerry02202 <155348042+Jerry02202@users.noreply.github.com>
2025-05-20 18:35:43 -0500

    Update check_price.py

--- check_price.py
+++ check_price.py
@@ -79,9 +79,9 @@ def check_product_price():
         soup = BeautifulSoup(response.content, "html.parser")
         
         # ----- INICIO DE IMPRESIÓN DEL HTML (SOLO PARA DEPURACIÓN) -----
-        # print("\n\n--- INICIO DEL HTML OBTENIDO ---\n")
-         print(soup.prettify()) # Descomentar esta línea temporalmente si las búsquedas siguen fallando
-        # print("\n--- FIN DEL HTML OBTENIDO ---\n\n")
+        print("\n\n--- INICIO DEL HTML OBTENIDO ---\n")
+        print(soup.prettify()) # Descomentar esta línea temporalmente si las búsquedas siguen fallando
+        print("\n--- FIN DEL HTML OBTENIDO ---\n\n")
         # ----- FIN DE IMPRESIÓN DEL HTML -----
 
         texto_regular, texto_promo, regular_tachado = find_price_elements(soup)
